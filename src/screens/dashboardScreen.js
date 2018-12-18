6d05d3b5289b050f0c80bf20767ea18febb23ba6
Sergiy.D <Golfersds@gmail.com>
2018-12-18 11:47:46 +0200

    change dashboard Button

--- src/screens/dashboardScreen.js
+++ src/screens/dashboardScreen.js
@@ -2,12 +2,11 @@ import React, {Component} from 'react';
 import {
   View,
   Text,
-  Button,
   StyleSheet,
 } from 'react-native';
-
+import { Button } from 'react-native-elements';
 import HeaderBlock from './../components/shared/header';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import FeatherIcon from 'react-native-vector-icons/Feather';
 
 
 class DashboardScreen extends Component {
@@ -17,13 +16,30 @@ class DashboardScreen extends Component {
         <HeaderBlock navigation={this.props.navigation}/>
 
         <Button
+          icon={
+            <FeatherIcon
+              name='user-check'
+              size={30}
+              color='#fff'
+            />
+          }
+          title="Positive aspect"
+          titleStyle={{ fontWeight: "900" }}
+          buttonStyle = {styles.buttonPositive}
           onPress={() => this.props.navigation.navigate('Positive')}
-          title="Go to Positive"
-          style = {styles.buttonPositive}
         />
         <Button
+          icon={
+            <FeatherIcon
+              name='user-x'
+              size={30}
+              color='#fff'
+            />
+          }
+          title="Negative aspect"
+          titleStyle={{ fontWeight: "900" }}
+          buttonStyle = {styles.buttonNegative}
           onPress={() => this.props.navigation.navigate('Negative')}
-          title="Go to Negative"
         />
 
       </View>
@@ -34,10 +50,24 @@ class DashboardScreen extends Component {
 
 const styles = StyleSheet.create({
   buttonPositive: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5F5F5',
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 10,
+    marginBottom: 5,
+    borderWidth: 0,
+    borderRadius: 5,
+    height: 250,
+    backgroundColor: "green",
+  },
+  buttonNegative: {
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 5,
+    marginBottom: 5,
+    borderWidth: 0,
+    borderRadius: 5,
+    height: 250,
+    backgroundColor: 'red',
   }
 })
 
